7ff49e59abbbeafed05227625e45101cfc36568c
PaSTKuN <past.kun@gmail.com>
2021-03-28 19:11:02 -0300

    adicionado npm

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ongaku</title>
+    <link rel="stylesheet" href="node_modules/materialize-css/dist/css/materialize.css">
+</head>
+<div class="container">
+    <div class="row">
+        <div class="col-md">
+            One of three columns
+        </div>
+        <div class="col-md">
+            One of three columns
+        </div>
+        <div class="col-md">
+            One of three columns
+        </div>
+    </div>
+</div>
+<body>
+<script src="node_modules/materialize-css/dist/js/materialize.js"></script>
+</body>
+</html>
