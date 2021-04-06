c9d2b414485e3e2a141778b205949a368e67c15b
Cristiano Mozena <past.kun@gmail.com>
2021-04-06 16:18:13 -0300

    Criado alguns arquivos de extrutura;

--- index.php
+++ index.php
@@ -2,25 +2,26 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ongaku</title>
     <link rel="stylesheet" href="node_modules/materialize-css/dist/css/materialize.css">
 </head>
-<div class="container">
-    <div class="row">
-        <div class="col-md">
-            One of three columns
-        </div>
-        <div class="col-md">
-            One of three columns
-        </div>
-        <div class="col-md">
-            One of three columns
-        </div>
-    </div>
-</div>
+<?php
+    $parm = 'home';
+    // get_pages
+    if (isset($_GET['teste'])){
+        $titulo = 'teste';
+        $parm = 'teste';
+
+    }
+
+    // Inclui Paginas conforme o get
+    switch ($parm){
+        case 'teste':
+            include_once 'teste.php';
+    }
+?>
 <body>
 <script src="node_modules/materialize-css/dist/js/materialize.js"></script>
 </body>
